b069dbb860a316b89893635d91957e3886e30030
Justin Cheung <Justin@justins-macbook-pro-2.local>
2015-10-29 01:03:58 +0800

    End of lecture 3/ initial version

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,14 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+
+    var typeInMiddle = false
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func digitInput(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if(typeInMiddle==false){
+            display.text = digit
+            typeInMiddle=true
+        }
+        else{
+            display.text = display.text! + digit
+            
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func operate(sender: UIButton) {
+        if typeInMiddle{
+            enter()
+        }
+        if let operation = sender.currentTitle{
+            if let result = brain.performOperation(operation){
+                displayValue = result
+            }
+            else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func enter() {
+        typeInMiddle = false
+        if let result = brain.pushOperand(displayValue){
+            displayValue = result
+        }
+        else{
+            displayValue = 0
+        }
+    }
+    
+
+    
+    var displayValue: Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text! = "\(newValue)"
+            typeInMiddle = false
+        }
     }
 
 
